51f83adbf1abaedc205ca750920fe8f0b6057802
soonwonchoi <soonwon.choi@samsung.com>
2025-07-02 10:13:19 +0900

    [refactoring] page 3

--- PrimeFactor/prime_factors.cpp
+++ PrimeFactor/prime_factors.cpp
@@ -6,13 +6,9 @@ public:
 	std::vector<int> of(int number)
 	{
 		std::vector<int> result = {};
-		if (number == 2)
+		if (number > 1)
 		{
-			result.push_back(2);
-		}
-		else if (number == 3)
-		{
-			result.push_back(3);
+			result.push_back(number);
 		}
 
 		return result;
